df988f64cc6b866b6986791796bbdeaac1c46675
Rosamunda <rosamunda@gmail.com>
2016-02-10 00:21:16 -0300

    agregado el archivo para un nuevo ejemplo

---
+++ js/conversorMonedas.js
@@ -0,0 +1,5 @@
+/*
+Hacer un programa en el que se ingrese un monto de dinero expresado en pesos y la 
+cotización del dólar y luego imprima por pantalla el monto equivalente en dólares 
+(con 2 dígitos decimales).
+*/
