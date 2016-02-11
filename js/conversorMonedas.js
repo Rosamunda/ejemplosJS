4f4ae2fbc560e550ee049e6d734c4a13db09de98
Rosamunda <rosamunda@gmail.com>
2016-02-11 18:14:32 -0300

    agregué el secript al conversor de monedas

--- js/conversorMonedas.js
+++ js/conversorMonedas.js
@@ -3,3 +3,10 @@ Hacer un programa en el que se ingrese un monto de dinero expresado en pesos y l
 cotización del dólar y luego imprima por pantalla el monto equivalente en dólares 
 (con 2 dígitos decimales).
 */
+
+function conversorMonedas() {
+	var pesos = document.getElementById("pesos");
+	var calcularDolar = pesos.value * 14.7;
+	document.getElementById("resultado").innerHTML = pesos.value + " pesos equivalen a " + calcularDolar + " dólares.";
+	return false;
+}
